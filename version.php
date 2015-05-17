ad5547c84465a0686c7bd9904d15fd1dc450445d
David Herney <davidherney@gmail.com>
2015-05-17 14:57:00 -0500

    Release candidate

--- version.php
+++ version.php
@@ -26,5 +26,5 @@ defined('MOODLE_INTERNAL') || die();
 $plugin->version = 2015043002;        // The current plugin version (Date: YYYYMMDDXX)
 $plugin->requires  = 2014050800;        // Requires this Moodle version.
 $plugin->component = 'format_menutopic';    // Full name of the plugin (used for diagnostics)
-$plugin->maturity = MATURITY_BETA;
+$plugin->maturity = MATURITY_RC;
 $plugin->release = 'ludi';
